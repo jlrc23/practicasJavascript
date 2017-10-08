d4b9360ad1393479988af143b7ef253037ef073a
Jose Luis Rosado <jose.rosado.consultant@nielsen.com>
2017-10-07 21:39:37 -0500

    Add remove items

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-let items = new Set([1,2,3,7,7,7,7]);
+let items = new Set([1,2,3,4,5]);
+console.log("Value items: ", items);
+console.log(items.size);
+items.delete(3);
+console.log(items.size);
+console.log("next delete 3: ", items);
+items.clear();
+console.log("Next delete all: ", items);
 
-console.log("Items has 7:", items.has(7));
-console.log("Items has '7'", items.has('7'));
-console.log(items);
---
+++ main2.js
@@ -0,0 +1,5 @@
+let items = new Set([1,2,3,7,7,7,7]);
+
+console.log("Items has 7:", items.has(7));
+console.log("Items has '7'", items.has('7'));
+console.log(items);
